2049811e5b96e4a20259df457589011b949c09bf
Sayyed Mohsen Vazirizade <s.m.vazirizade@gmail.com>
2021-06-13 21:02:52 -0500

    adding a recursive method
    1) adding kth_to_last_recursive
    2) adding a new test case
    3) adding a for loop to incorporate both methods

--- chapter_02/p02_return_kth_to_last.py
+++ chapter_02/p02_return_kth_to_last.py
@@ -1,7 +1,7 @@
 from chapter_02.linked_list import LinkedList
 
 
-def kth_to_last(ll, k):
+def kth_to_last_runner(ll, k):
     runner = current = ll.head
     for _ in range(k):
         if not runner:
@@ -15,18 +15,50 @@ def kth_to_last(ll, k):
     return current
 
 
+
+def kth_to_last_recursive(ll, k):
+    def Recursion(head, k):
+        if head is None:
+            return 0,None
+        else:
+            i,Node=Recursion(head.next, k)
+            i+=1
+            if i==k:
+                return i,head
+            else:
+                return i, Node     
+    
+    if ll is None:
+        return None
+    else:
+        head=ll.head
+        i, Node=Recursion(head, k)
+        return Node
+
+
+
+
+
 test_cases = (
     # list, k, expected
     ((10, 20, 30, 40, 50), 1, 50),
+    ((10, 20, 30, 40, 50), 3, 30),
     ((10, 20, 30, 40, 50), 5, 10),
 )
 
 
+Method_cases=[kth_to_last_runner,kth_to_last_recursive]
+
+#Method_cases=[kth_to_last_recursive]
+
 def test_kth_to_last():
-    for linked_list_values, k, expected in test_cases:
-        ll = LinkedList(linked_list_values)
-        assert kth_to_last(ll, k).value == expected
+    for kth_to_last in Method_cases:
+        for linked_list_values, k, expected in test_cases:
+            ll = LinkedList(linked_list_values)
+            assert kth_to_last(ll, k).value == expected
 
 
 if __name__ == "__main__":
     test_kth_to_last()
+           
+
