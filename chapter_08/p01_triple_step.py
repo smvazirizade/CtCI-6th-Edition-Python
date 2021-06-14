350655c728bfb1078975ac248634c27744a682c7
Sayyed Mohsen Vazirizade <s.m.vazirizade@gmail.com>
2021-06-13 23:33:17 -0500

    adding more methods and case tests
    1) adding 2 methods, Top_Down_Dict and Bottom_Up
    2) changing the name of the method_2 to Top_Down_List and combining the 2 previous functions in one
    3) adding a similar way to chapter 1 for testing

--- chapter_08/p01_triple_step.py
+++ chapter_08/p01_triple_step.py
@@ -1,4 +1,6 @@
+
 def triple_hop(x):
+    #Recursive and no memozation
     if x < 0:
         return 0
     if x == 0:
@@ -8,36 +10,83 @@ def triple_hop(x):
     return triple_hop(x - 1) + triple_hop(x - 2) + triple_hop(x - 3)
 
 
-def method_2(x):
-    memo = [-1] * (x + 1)
-    return triple_hop_recursive(x, memo)
 
 
-def triple_hop_recursive(x, memo):
-    if x < 0:
+def Top_Down_List(N,Memo=None):
+    #Dynamic programing with memozation using list and for loop
+    if N<0:
         return 0
-    memo[0] = 1
-    if x >= 1:
-        memo[1] = 1
-    if x >= 2:
-        memo[2] = memo[1] + memo[0]
-    if x > 2:
-        for i in range(3, x + 1):
-            memo[i] = memo[i - 1] + memo[i - 2] + memo[i - 3]
-    return memo[x]
+    if Memo is None:
+        Memo=[-1]*(N+1)
+        if N>=0:
+            Memo[0]=1
+        if N>=1:
+            Memo[1]=1
+        if N>=2:
+            Memo[2]=2
+    for i in range(3,N+1):
+        Memo[i] = Memo[i-1] + Memo[i-2] + Memo[i-3]
+    return Memo[N]
+        
+
+
+def Top_Down_Dict(N,Memo={0:1,1:1,2:2}):
+    #Dynamic programing with memozation using dictionary and recursion
+    if N<0:
+        return 0
+    if N in Memo.keys():
+        return Memo[N]
+    else:
+        Memo[N] = Top_Down_Dict(N-1,Memo)+Top_Down_Dict(N-2,Memo)+Top_Down_Dict(N-3,Memo)
+        return Memo[N]
+        
+        
+        
+def Bottom_Up(N):
+    #Dynamic programing 
+    a=1
+    b=1
+    c=2
+    if N<0:
+        return 0
+    if N==0:
+        return a
+    if N==1:
+        return b
+    if N==2:
+        return c
+    if N>2:
+        for i in range(3,N+1):
+            d=a+b+c
+            a=b
+            b=c
+            c=d
+        return d
+    
+N=100
+Bottom_Up(N)
+
+
+
+
+test_cases = (
+    # list, k, expected
+    (1,1),
+    (2,2),
+    (3,4),
+    (10,274))
+
+
+Method_cases=[triple_hop,Top_Down_List,Top_Down_List, Top_Down_Dict,Bottom_Up]
+
+#Method_cases=[kth_to_last_recursive]
+
+def Triple_Steps():
+    for Method in Method_cases:
+        for N,expected in test_cases:
+            print(N, Method(N))
+            #assert Method(N) == expected
 
 
 if __name__ == "__main__":
-    print(triple_hop(1))
-    print(triple_hop(2))
-    print(triple_hop(3))
-    print(triple_hop(4))
-    print(triple_hop(5))
-    print(triple_hop(6))
-
-    print(method_2(1))
-    print(method_2(2))
-    print(method_2(3))
-    print(method_2(4))
-    print(method_2(5))
-    print(method_2(6))
+    Triple_Steps()
